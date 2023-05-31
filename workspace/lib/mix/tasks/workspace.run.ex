4864a46c937c32b067b1a51154e49a0e473c8809
pnezis <pnezis@gmail.com>
2023-05-31 13:51:25 +0300

    feat: support execution of tasks via Ports

--- workspace/lib/mix/tasks/workspace.run.ex
+++ workspace/lib/mix/tasks/workspace.run.ex
@@ -1,25 +1,25 @@
 defmodule Mix.Tasks.Workspace.Run do
+  @options_schema Workspace.Cli.global_opts()
+
   @shortdoc "Run a mix command to all projects"
 
   @moduledoc """
 
   ## Command Line Options
 
-  #{CliOpts.docs(Workspace.Cli.global_opts())}
+  #{CliOpts.docs(@options_schema)}
   """
 
   use Mix.Task
 
+  alias Workspace.Cli
+
   def run(argv) do
-    {args, argv, extra_args} =
-      case CliOpts.validate(argv, Workspace.Cli.global_opts()) do
-        {:error, reason} -> Mix.raise(reason)
-        args -> args
-      end
+    %{parsed: parsed, args: args, extra: extra} = CliOpts.parse!(argv, @options_schema)
 
     Workspace.projects()
-    |> Workspace.Cli.filter_projects(args, argv)
-    |> Enum.each(fn project -> run_in_project(project, args, extra_args) end)
+    |> Workspace.Cli.filter_projects(parsed, args)
+    |> Enum.each(fn project -> run_in_project(project, parsed, extra) end)
   end
 
   defp run_in_project(%{skip: true, app: app}, args, _argv) do
@@ -28,11 +28,60 @@ defmodule Mix.Tasks.Workspace.Run do
     end
   end
 
-  defp run_in_project(project, args, argv) do
-    WorkspaceColors.info("#{args[:task]}", "#{project[:app]}")
+  defp run_in_project(project, options, argv) do
+    task = options[:task]
 
-    Mix.Project.in_project(project[:app], Path.dirname(project[:path]), fn _mixfile ->
-      Mix.Task.run(args[:task], argv)
-    end)
+    project_relative_path = Workspace.relative_path(Path.dirname(project[:path]))
+    task_args = [task | argv]
+
+    Cli.info(project_relative_path, "- mix #{Enum.join(task_args, " ")}", prefix: "==> ")
+
+    case options[:execution_mode] do
+      "process" ->
+        cmd(task, argv, Path.dirname(project[:path]))
+
+      "subtask" ->
+        Mix.Project.in_project(project[:app], Path.dirname(project[:path]), fn _mixfile ->
+          Mix.Task.run(task, argv)
+        end)
+
+      other ->
+        Mix.raise("invalid execution mode #{other}, only `process` and `subtask` are supported")
+    end
+  end
+
+  defp cmd(task, argv, path) do
+    mix = System.find_executable("mix")
+    args = [task | argv]
+
+    port =
+      Port.open({:spawn_executable, mix}, [
+        :stream,
+        :hide,
+        :use_stdio,
+        :stderr_to_stdout,
+        :binary,
+        :exit_status,
+        args: args,
+        cd: path
+      ])
+
+    stream_output([args: args, path: path], port)
+  end
+
+  defp stream_output(meta, port) do
+    receive do
+      {^port, {:data, data}} ->
+        IO.write(data)
+        stream_output(meta, port)
+
+      {^port, {:exit_status, 0}} ->
+        0
+
+      {^port, {:exit_status, status}} ->
+        args = Keyword.get(meta, :args)
+        path = Keyword.get(meta, :path)
+        IO.puts("(under #{path}) `mix #{Enum.join(args, " ")}` failed with status #{status}")
+    end
   end
 end
--- workspace/lib/workspace/cli.ex
+++ workspace/lib/workspace/cli.ex
@@ -17,10 +17,21 @@ defmodule Workspace.Cli do
       doc: "The task to execute",
       required: true
     ],
-    mode: [
+    execution_order: [
       type: :string,
-      default: :serial,
-      doc: "The execution mode, one of `serial`, `parallel`, `roots`, `bottom-up`"
+      default: "serial",
+      doc: "The execution order, one of `serial`, `parallel`, `roots`, `bottom-up`"
+    ],
+    execution_mode: [
+      type: :string,
+      default: "process",
+      doc: """
+      The execution mode. It supports the following values:
+
+        - `process` - every subcommand will be executed as a different process, this
+        is the preferred mode for most mix tasks
+        - `subtask` - invokes `Mix.Task.run` from the workspace in the given project
+      """
     ],
     verbose: [
       type: :boolean,
@@ -35,10 +46,7 @@ defmodule Workspace.Cli do
   def global_opts, do: @global_cli_opts
 
   def filter_projects(projects, args, argv) do
-    ignored =
-      args
-      |> Keyword.get_values(:ignore)
-      |> Enum.map(&String.to_atom/1)
+    ignored = Enum.map(args[:ignore], &String.to_atom/1)
 
     selected = Enum.map(argv, &String.to_atom/1)
 
@@ -49,4 +57,63 @@ defmodule Workspace.Cli do
 
   defp skippable?(%{app: app}, [], ignored), do: app in ignored
   defp skippable?(%{app: app}, selected, ignored), do: app not in selected || app in ignored
+
+  @doc """
+  Prints an info (blue) message
+
+  See also `WorkspaceColors.log/4`
+  """
+  @spec info(command :: String.t(), message :: IO.ANSI.ansidata(), opts :: Keyword.t()) :: :ok
+  def info(command, message, opts \\ []), do: log(:blue, command, message, opts)
+
+  @doc """
+  Prints a success (green) message
+
+  See also `WorkspaceColors.log/4`
+  """
+  @spec success(command :: String.t(), message :: IO.ANSI.ansidata(), opts :: Keyword.t()) :: :ok
+  def success(command, message, opts \\ []), do: log(:green, command, message, opts)
+
+  @doc """
+  Prints an error (red) message
+
+  See also `WorkspaceColros.log/4`
+  """
+  @spec error(command :: String.t(), message :: IO.ANSI.ansidata(), opts :: Keyword.t()) :: :ok
+  def error(command, message, opts \\ []), do: log(:red, command, message, opts)
+
+  @doc """
+  Prints a warning (yellow) message
+
+  See also `Mix.Helpers.log/4`
+  """
+  @spec warning(command :: String.t(), message :: IO.ANSI.ansidata(), opts :: Keyword.t()) :: :ok
+  def warning(command, message, opts \\ []), do: log(:yellow, command, message, opts)
+
+  @doc """
+  Generic log message with a colored part
+
+  The command part is colored with the given `color`. You can specify in the
+  `opts` list the `prefix` which defaults to (`* `) and `suffix` (which
+  defaults to ` `) in order to modify the default message appearance.
+
+  Optionally you can pass an `ansilist` similarly to `c:Mix.Shell.info/1` for
+  custom formatting of the `message`.
+  """
+  @spec log(
+          color :: atom(),
+          command :: String.t(),
+          message :: IO.ANSI.ansidata(),
+          opts :: Keyword.t()
+        ) ::
+          :ok
+  def log(color, command, message, opts) when is_binary(message) do
+    log(color, command, [message], opts)
+  end
+
+  def log(color, command, message, opts) when is_list(message) do
+    prefix = Keyword.get(opts, :prefix, "* ")
+    suffix = Keyword.get(opts, :suffix, " ")
+    Mix.shell().info([color, "#{prefix}#{command}#{suffix}", :reset | message])
+  end
 end
