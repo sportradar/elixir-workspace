0e50c3919794149b3be89803ee82a71a00bb0303
pnezis <pnezis@gmail.com>
2023-06-16 17:52:46 +0300

    test: more tests

---
+++ workspace/test/mix/tasks/workspace.graph_test.exs
@@ -0,0 +1,28 @@
+defmodule Mix.Tasks.Workspace.GraphTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  @sample_workspace_path "test/fixtures/sample_workspace"
+
+  test "prints the tree of the workspace" do
+    expected = """
+    project_a
+    ├── project_b
+    │   └── project_g
+    ├── project_c
+    │   ├── project_e
+    │   └── project_f
+    │       └── project_g
+    └── project_d
+    project_h
+    └── project_d
+    project_i
+    └── project_j
+    project_k
+    """
+
+    assert capture_io(fn ->
+             Mix.Tasks.Workspace.Graph.run(["--workspace-path", @sample_workspace_path])
+           end) == expected
+  end
+end
