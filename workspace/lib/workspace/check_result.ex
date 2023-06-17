ad5a09ea47ea69e81785abacdfc2bf8a70a6c404
pnezis <pnezis@gmail.com>
2023-06-17 20:22:02 +0300

    polish CheckResult

--- workspace/lib/workspace/check_result.ex
+++ workspace/lib/workspace/check_result.ex
@@ -1,9 +1,25 @@
 defmodule Workspace.CheckResult do
+  @moduledoc """
+  A checker's result
+
+  Contains info about the checked project if applicable, the checker config
+  and the check status and metadata.
+  """
+
+  alias __MODULE__
+
+  @type t :: %CheckResult{
+    checker: module(),
+    project: atom(),
+    status: :ok | :error | :skip,
+    meta: keyword(),
+    index: pos_integer()
+  }
+
   defstruct checker: nil,
             project: nil,
             status: nil,
-            error: nil,
-            # should be set by checkers on failure, used for printing errors
+            # should be set by checkers
             meta: [],
             # TODO: remove instead pass the check to the struct
             index: nil
