1116b29944c59a0a6d1783f3457b11f8582377cb
pnezis <pnezis@gmail.com>
2023-06-23 13:31:15 +0300

    chore: fix module names in workspace.exs

--- .workspace.exs
+++ .workspace.exs
@@ -6,15 +6,16 @@
   # TODO: print ignored and successful only with verbose
   checks: [
     [
-      module: Workspace.Checkers.ValidateConfigPath,
+      module: Workspace.Checks.ValidateConfigPath,
       description: "all projects must have a common dependencies path",
+      ignore: [:cli_opts],
       opts: [
         config_attribute: :deps_path,
         expected_path: "artifacts/deps"
       ]
     ],
     [
-      module: Workspace.Checkers.ValidateConfigPath,
+      module: Workspace.Checks.ValidateConfigPath,
       description: "all projects must have a common build path",
       opts: [
         config_attribute: :build_path,
