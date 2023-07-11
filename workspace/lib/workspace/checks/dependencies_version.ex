a1579e5451bb04604596e63c2b20a42f286b4049
pnezis <pnezis@gmail.com>
2023-07-11 16:28:59 +0300

    refactor and improve output of dependencies check

--- workspace/lib/workspace/checks/dependencies_version.ex
+++ workspace/lib/workspace/checks/dependencies_version.ex
@@ -35,50 +35,83 @@ defmodule Workspace.Checks.DependenciesVersion do
 
   @impl Workspace.Check
   def check(workspace, check) do
-    deps =
+    expected_deps =
       Keyword.fetch!(check[:opts], :deps)
-      |> deps_to_keyword()
+      |> parse_deps()
+      |> Enum.map(fn {dep, version, opts} -> {dep, {version, opts}} end)
 
     Workspace.Check.check_projects(workspace, check, fn project ->
-      check_dependencies_versions(project, deps)
+      check_dependencies_versions(project, expected_deps)
     end)
   end
 
-  defp check_dependencies_versions(project, deps) do
-    configured_deps = deps_to_keyword(project.config[:deps])
+  defp check_dependencies_versions(project, expected_deps) do
+    configured_deps = parse_deps(project.config[:deps])
 
     mismatches =
       configured_deps
-      |> Enum.filter(fn {dep, options} -> dep_mismatch?(dep, options, deps) end)
-      |> Enum.map(&elem(&1, 0))
+      |> Enum.map(fn dep -> check_dependency_version(dep, expected_deps) end)
+      |> Enum.filter(fn result ->
+        case result do
+          {:error, _dep, _message} -> true
+          :ok -> false
+        end
+      end)
+      |> Enum.map(fn {:error, dep, message} -> {dep, message} end)
 
     case mismatches do
-      [] -> {:ok, check_metadata(mismatches, configured_deps, deps)}
-      mismatches -> {:error, check_metadata(mismatches, configured_deps, deps)}
+      [] -> {:ok, check_metadata(mismatches, configured_deps, expected_deps)}
+      mismatches -> {:error, check_metadata(mismatches, configured_deps, expected_deps)}
     end
   end
 
-  defp deps_to_keyword(deps), do: Enum.map(deps, &split_dep_tuple/1)
+  defp parse_deps(deps), do: Enum.map(deps, &split_dep_tuple/1)
 
   defp split_dep_tuple(dep) do
     dep_name = elem(dep, 0)
     rest = Tuple.delete_at(dep, 0)
 
-    {dep_name, rest}
+    case rest do
+      {version, opts} when is_binary(version) and is_list(opts) ->
+        {dep_name, version, opts}
+
+      {version} when is_binary(version) ->
+        {dep_name, version, []}
+
+      {opts} when is_list(opts) ->
+        {dep_name, nil, opts}
+    end
   end
 
-  defp dep_mismatch?(dep, options, expected) do
-    case Keyword.has_key?(expected, dep) do
-      true -> expected[dep] != options
-      false -> false
+  defp check_dependency_version({dep, version, options}, expected) do
+    if Keyword.has_key?(expected, dep) do
+      {expected_version, expected_options} = expected[dep]
+
+      cond do
+        expected_version != version ->
+          {:error, dep,
+           "#{inspect(dep)} - expected version: #{expected_version}, got: #{version}"}
+
+        expected_options != options ->
+          {:error, dep,
+           "#{inspect(dep)} - expected options: #{inspect(expected_options)}, got: #{inspect(options)}"}
+
+        true ->
+          :ok
+      end
+    else
+      :ok
     end
   end
 
   defp check_metadata(mismatches, configured, expected) do
+    mismatched_deps = Enum.map(mismatches, fn {dep, _message} -> dep end)
+    configured = Enum.map(configured, fn {dep, version, opts} -> {dep, {version, opts}} end)
+
     [
-      mismatches: mismatches,
-      configured: Keyword.take(configured, mismatches),
-      expected: Keyword.take(expected, mismatches)
+      mismatches: mismatched_deps,
+      configured: Keyword.take(configured, mismatched_deps),
+      expected: Keyword.take(expected, mismatched_deps)
     ]
   end
 
@@ -87,12 +120,34 @@ defmodule Workspace.Checks.DependenciesVersion do
         status: :error,
         meta: meta
       }) do
-    [
+    main_line = [
       "version mismatches for the following dependencies: ",
-      :light_cyan,
+      :yellow,
       inspect(meta[:mismatches]),
       :reset
     ]
+
+    details =
+      meta[:mismatches]
+      |> Enum.map(fn dep ->
+        [
+          "\n",
+          "     ",
+          :yellow,
+          inspect(dep),
+          :reset,
+          " expected ",
+          :light_cyan,
+          inspect(meta[:expected][dep]),
+          :reset,
+          " got ",
+          :light_cyan,
+          inspect(meta[:configured][dep]),
+          :reset
+        ]
+      end)
+
+    Enum.concat([main_line | details])
   end
 
   def format_result(%Workspace.Check.Result{status: :ok}) do
