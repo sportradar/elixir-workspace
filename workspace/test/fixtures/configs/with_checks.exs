939e8804a406749d1966fa415a39ed132058d31b
pnezis <pnezis@gmail.com>
2023-07-18 14:15:01 +0300

    extend check task tests

--- workspace/test/fixtures/configs/with_checks.exs
+++ workspace/test/fixtures/configs/with_checks.exs
@@ -7,6 +7,35 @@
         config_attribute: :deps_path,
         expected_path: "deps"
       ]
+    ],
+    [
+      module: Workspace.Checks.ValidateConfig,
+      description: "fail on package b",
+      opts: [
+        validate: fn config ->
+          case config[:app] do
+            :package_b -> {:error, "invalid package"}
+            :package_f -> {:ok, ""}
+            _other -> {:ok, "no error"}
+          end
+        end
+      ],
+      allow_failure: [:package_b]
+    ],
+    [
+      module: Workspace.Checks.ValidateConfig,
+      description: "always fails",
+      opts: [
+        validate: fn _config -> {:error, "this always fails"} end
+      ],
+      allow_failure: true
+    ],
+    [
+      module: Workspace.Checks.ValidateConfig,
+      description: "never fails",
+      opts: [
+        validate: fn _config -> {:ok, "never fails"} end
+      ]
     ]
   ]
 ]
--- workspace/test/mix/tasks/workspace.check_test.exs
+++ workspace/test/mix/tasks/workspace.check_test.exs
@@ -10,10 +10,6 @@ defmodule Mix.Tasks.Workspace.CheckTest do
 
   setup do
     Application.put_env(:elixir, :ansi_enabled, false)
-
-    # on_exit(fn ->
-    #   Application.put_env(:elixir, :ansi_enabled, true)
-    # end)
   end
 
   test "raises if no checks are defined" do
@@ -24,7 +20,7 @@ defmodule Mix.Tasks.Workspace.CheckTest do
 
   test "runs all configured checks" do
     expected = [
-      "==> running 1 workspace checks on the workspace",
+      "==> running 4 workspace checks on the workspace",
       "==> C000 check deps_path",
       "ERROR :package_a - expected :deps_path to be ../deps, got: deps",
       "ERROR :package_b - expected :deps_path to be ../deps, got: deps",
@@ -35,7 +31,22 @@ defmodule Mix.Tasks.Workspace.CheckTest do
       "ERROR :package_g - expected :deps_path to be ../deps, got: deps",
       "ERROR :package_i - expected :deps_path to be ../deps, got: deps",
       "ERROR :package_j - expected :deps_path to be ../deps, got: deps",
-      "ERROR :package_k - expected :deps_path to be ../deps, got: deps"
+      "ERROR :package_k - expected :deps_path to be ../deps, got: deps",
+      "==> C001 fail on package b",
+      "WARN  :package_b - invalid package",
+      "==> C002 always fails",
+      "WARN  :package_a - this always fails",
+      "WARN  :package_b - this always fails",
+      "WARN  :package_c - this always fails",
+      "WARN  :package_d - this always fails",
+      "WARN  :package_e - this always fails",
+      "WARN  :package_f - this always fails",
+      "WARN  :package_g - this always fails",
+      "WARN  :package_h - this always fails",
+      "WARN  :package_i - this always fails",
+      "WARN  :package_j - this always fails",
+      "WARN  :package_k - this always fails",
+      "==> C003 never fails"
     ]
 
     captured =
@@ -48,6 +59,79 @@ defmodule Mix.Tasks.Workspace.CheckTest do
             @sample_workspace_path,
             "--config-path",
             "../configs/with_checks.exs"
+          ])
+        end
+      )
+
+    assert_cli_output_match(captured, expected)
+  end
+
+  test "with --verbose flag on" do
+    expected = [
+      "==> running 4 workspace checks on the workspace",
+      "==> C000 check deps_path",
+      "ERROR :package_a - expected :deps_path to be ../deps, got: deps test/fixtures/sample_workspace/package_a",
+      "ERROR :package_b - expected :deps_path to be ../deps, got: deps",
+      "ERROR :package_c - expected :deps_path to be ../deps, got: deps",
+      "SKIP  :package_d - check skipped",
+      "ERROR :package_e - expected :deps_path to be ../deps, got: deps",
+      "ERROR :package_f - expected :deps_path to be ../deps, got: deps",
+      "ERROR :package_g - expected :deps_path to be ../deps, got: deps",
+      "OK    :package_h - :deps_path is set to ../deps",
+      "ERROR :package_i - expected :deps_path to be ../deps, got: deps",
+      "ERROR :package_j - expected :deps_path to be ../deps, got: deps",
+      "ERROR :package_k - expected :deps_path to be ../deps, got: deps",
+      "==> C001 fail on package b",
+      "OK    :package_a - no error",
+      "WARN  :package_b - invalid package",
+      "OK    :package_c - no error",
+      "SKIP  :package_d - check skipped",
+      "OK    :package_e - no error",
+      "OK    :package_f",
+      "OK    :package_g - no error",
+      "OK    :package_h - no error",
+      "OK    :package_i - no error",
+      "OK    :package_j - no error",
+      "OK    :package_k - no error",
+      "==> C002 always fails",
+      "WARN  :package_a - this always fails",
+      "WARN  :package_b - this always fails",
+      "WARN  :package_c - this always fails",
+      "SKIP  :package_d - check skipped",
+      "WARN  :package_e - this always fails",
+      "WARN  :package_f - this always fails",
+      "WARN  :package_g - this always fails",
+      "WARN  :package_h - this always fails",
+      "WARN  :package_i - this always fails",
+      "WARN  :package_j - this always fails",
+      "WARN  :package_k - this always fails",
+      "==> C003 never fails",
+      "OK    :package_a - never fails",
+      "OK    :package_b - never fails",
+      "OK    :package_c - never fails",
+      "SKIP  :package_d - check skipped",
+      "OK    :package_e - never fails",
+      "OK    :package_f - never fails",
+      "OK    :package_g - never fails",
+      "OK    :package_h - never fails",
+      "OK    :package_i - never fails",
+      "OK    :package_j - never fails",
+      "OK    :package_k - never fails"
+    ]
+
+    captured =
+      assert_raise_and_capture_io(
+        Mix.Error,
+        ~r"mix workspace.check failed - errors detected in 1 checks",
+        fn ->
+          CheckTask.run([
+            "--workspace-path",
+            @sample_workspace_path,
+            "--config-path",
+            "../configs/with_checks.exs",
+            "--verbose",
+            "--ignore",
+            "package_d"
           ])
         end
       )
