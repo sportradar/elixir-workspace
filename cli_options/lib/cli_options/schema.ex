6faa01c34f63f4b2452698ab4ee639bcbda7b325
pnezis <pnezis@gmail.com>
2024-04-08 15:57:37 +0300

    Schema validation in cli_options

--- cli_options/lib/cli_options/schema.ex
+++ cli_options/lib/cli_options/schema.ex
@@ -14,17 +14,149 @@ defmodule CliOptions.Schema do
 
   # validates the schema, rename to new! similar to nimbleoptiosn
   def new!(schema) do
-    # TODO: proper schema validation
+    if not Keyword.keyword?(schema) do
+      raise ArgumentError, "schema was expected to be a keyword list, got: #{inspect(schema)}"
+    end
+
+    schema = validate_options!(schema)
 
     mappings = build_mappings(schema)
 
     %__MODULE__{schema: schema, mappings: mappings}
   end
 
+  # we are manually validating the schema for now, could be delegated to NimbleOptions
+  # but we want to avoid external dependencies. If it goes out of hand we may consider
+  # refactoring it accordingly
+  defp validate_options!(schema) do
+    for {option, opts} <- schema do
+      validate_option!(option, opts)
+    end
+  end
+
+  defp validate_option!(option, opts) do
+    with :ok <- validate_keys(Keyword.keys(opts)),
+         {:ok, opts} <- validate_type(opts),
+         {:ok, opts} <- validate_names(option, opts),
+         :ok <- ensure_boolean_or_nil(opts[:required], :required),
+         :ok <- ensure_boolean_or_nil(opts[:multiple], :multiple),
+         :ok <- ensure_binary_or_nil(opts[:doc], :doc),
+         {:ok, opts} <- validate_default_value(opts) do
+      {option, opts}
+    else
+      {:error, reason} -> raise ArgumentError, "invalid schema for :#{option}, #{reason}"
+    end
+  end
+
+  @valid_option_keys [
+    :type,
+    :long,
+    :short,
+    :doc,
+    :default,
+    :required,
+    :multiple,
+    :aliases,
+    :short_aliases
+  ]
+  defp validate_keys(keys) do
+    invalid_keys = keys -- @valid_option_keys
+
+    case invalid_keys do
+      [] -> :ok
+      invalid -> {:error, "the following schema keys are not supported: #{inspect(invalid)}"}
+    end
+  end
+
+  @valid_types [:string, :boolean, :integer, :float, :counter, :atom]
+
+  defp validate_type(opts) do
+    opts = Keyword.put_new(opts, :type, :string)
+    type = Keyword.fetch!(opts, :type)
+
+    case type in @valid_types do
+      true -> {:ok, opts}
+      false -> {:error, "invalid type #{inspect(type)}"}
+    end
+  end
+
+  defp validate_names(option, opts) do
+    opts =
+      opts
+      |> Keyword.put_new(:long, default_long_name(option))
+      |> Keyword.put_new(:aliases, [])
+      |> Keyword.put_new(:short_aliases, [])
+
+    with :ok <- ensure_binary(opts[:long], :long),
+         :ok <- ensure_binary_or_nil(opts[:short], :short),
+         :ok <- ensure_binary_list(opts[:aliases], :aliases),
+         :ok <- ensure_binary_list(opts[:short_aliases], :short_aliases) do
+      {:ok, opts}
+    end
+  end
+
+  defp default_long_name(option), do: Atom.to_string(option) |> String.replace("_", "-")
+
+  defp ensure_binary_or_nil(nil, _name), do: :ok
+  defp ensure_binary_or_nil(value, name), do: ensure_binary(value, name)
+
+  defp ensure_binary(value, _name) when is_binary(value), do: :ok
+
+  defp ensure_binary(value, name),
+    do: {:error, "#{inspect(name)} should be a string, got: #{inspect(value)}"}
+
+  defp ensure_binary_list([], _name), do: :ok
+
+  defp ensure_binary_list([head | rest], name) do
+    case ensure_binary(head, name) do
+      :ok ->
+        ensure_binary_list(rest, name)
+
+      _error ->
+        {:error,
+         "#{inspect(name)} expected a list of strings, got a non string item: #{inspect(head)}"}
+    end
+  end
+
+  defp ensure_binary_list(value, name),
+    do: {:error, "#{inspect(name)} expected a list of strings, got: #{inspect(value)}"}
+
+  defp ensure_boolean_or_nil(nil, _name), do: :ok
+  defp ensure_boolean_or_nil(value, _name) when is_boolean(value), do: :ok
+
+  defp ensure_boolean_or_nil(value, name),
+    do: {:error, "#{inspect(name)} should be boolean, got: #{inspect(value)}"}
+
+  defp validate_default_value(opts) do
+    opts = maybe_add_default_value(opts, opts[:type])
+
+    case validate_type_match(opts[:type], opts[:default]) do
+      true ->
+        {:ok, opts}
+
+      false ->
+        {:error,
+         ":default should be of type #{inspect(opts[:type])}, got: #{inspect(opts[:default])}"}
+    end
+  end
+
+  defp maybe_add_default_value(opts, :boolean), do: Keyword.put_new(opts, :default, false)
+  defp maybe_add_default_value(opts, :counter), do: Keyword.put_new(opts, :default, 0)
+  defp maybe_add_default_value(opts, _other), do: opts
+
+  defp validate_type_match(_type, nil), do: true
+
+  defp validate_type_match(:integer, value), do: is_integer(value)
+  defp validate_type_match(:counter, value), do: is_integer(value)
+  defp validate_type_match(:string, value), do: is_binary(value)
+  defp validate_type_match(:float, value), do: is_float(value)
+  defp validate_type_match(:atom, value), do: is_atom(value)
+  defp validate_type_match(:boolean, value), do: is_boolean(value)
+
   defp build_mappings(schema) do
     mappings = %{}
 
-    for {option, opts} <- schema, mapping <- option_mappings(option, opts), reduce: mappings do
+    for {option, opts} <- schema, mapping <- option_mappings(opts), reduce: mappings do
       mappings when is_map_key(mappings, mapping) ->
         raise ArgumentError,
               "mapping #{mapping} for option :#{option} is already defined for :#{mappings[mapping]}"
@@ -34,24 +166,14 @@ defmodule CliOptions.Schema do
     end
   end
 
-  defp option_mappings(option, opts) do
+  defp option_mappings(opts) do
     [
-      long_name(opts, option),
+      opts[:long],
       opts[:short]
     ]
-    |> Enum.concat(opts[:aliases] || [])
-    |> Enum.concat(opts[:short_aliases] || [])
+    |> Enum.concat(opts[:aliases])
+    |> Enum.concat(opts[:short_aliases])
     |> Enum.reject(&is_nil/1)
-  end
-
-  defp long_name(opts, option) do
-    case opts[:long] do
-      nil ->
-        Atom.to_string(option) |> String.replace("_", "-")
-
-      long ->
-        long
-    end
   end
 
   # opts is expected to be a keyword of the form [option: args]
@@ -83,15 +205,7 @@ defmodule CliOptions.Schema do
   end
 
   defp value_or_default(value, _schema) when not is_nil(value), do: value
-
-  defp value_or_default(nil, schema) do
-    cond do
-      schema[:default] != nil -> schema[:default]
-      schema[:type] == :boolean -> false
-      schema[:type] == :counter -> 0
-      true -> nil
-    end
-  end
+  defp value_or_default(nil, schema), do: schema[:default]
 
   defp validate_value(option, value, schema) do
     cond do
@@ -158,12 +272,8 @@ defmodule CliOptions.Schema do
     opts = Keyword.fetch!(schema, option)
 
     type = opts[:type]
-    action = opts[:action]
 
     cond do
-      action != nil ->
-        action
-
       type == :boolean ->
         :set_true
 
--- cli_options/test/cli_options/schema_test.exs
+++ cli_options/test/cli_options/schema_test.exs
@@ -2,6 +2,138 @@ defmodule CliOptions.SchemaTest do
   use ExUnit.Case
 
   describe "new!" do
+    test "with non keyword schema" do
+      assert_raise ArgumentError, "schema was expected to be a keyword list, got: \"foo\"", fn ->
+        CliOptions.Schema.new!("foo")
+      end
+    end
+
+    test "with invalid schemas" do
+      # invalid keys
+      schema = [foo: [type: :string, missing: false, other: 1]]
+
+      message =
+        "invalid schema for :foo, the following schema keys are not supported: [:missing, :other]"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      # invalid type
+      schema = [foo: [type: :stringlist]]
+      message = "invalid schema for :foo, invalid type :stringlist"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      # non string names
+      schema = [foo: [long: 1]]
+      message = "invalid schema for :foo, :long should be a string, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [short: 1]]
+      message = "invalid schema for :foo, :short should be a string, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [aliases: [nil]]]
+
+      message =
+        "invalid schema for :foo, :aliases expected a list of strings, got a non string item: nil"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [short_aliases: [:x]]]
+
+      message =
+        "invalid schema for :foo, :short_aliases expected a list of strings, got a non string item: :x"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [aliases: 1]]
+      message = "invalid schema for :foo, :aliases expected a list of strings, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      # invalid boolean flags
+      schema = [foo: [required: 1]]
+      message = "invalid schema for :foo, :required should be boolean, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [multiple: 1]]
+      message = "invalid schema for :foo, :multiple should be boolean, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      # invalid strings
+      schema = [foo: [doc: 1]]
+      message = "invalid schema for :foo, :doc should be a string, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      # invalid default values
+      schema = [foo: [type: :string, default: 1]]
+      message = "invalid schema for :foo, :default should be of type :string, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [type: :boolean, default: 1]]
+      message = "invalid schema for :foo, :default should be of type :boolean, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [type: :integer, default: :x]]
+      message = "invalid schema for :foo, :default should be of type :integer, got: :x"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [type: :counter, default: :x]]
+      message = "invalid schema for :foo, :default should be of type :counter, got: :x"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [type: :atom, default: 1]]
+      message = "invalid schema for :foo, :default should be of type :atom, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+
+      schema = [foo: [type: :float, default: 1]]
+      message = "invalid schema for :foo, :default should be of type :float, got: 1"
+
+      assert_raise ArgumentError, message, fn ->
+        CliOptions.Schema.new!(schema)
+      end
+    end
+
     test "with duplicate aliases" do
       # option name and long conflict
       schema = [foo: [type: :string], bar: [type: :string, long: "foo"]]
