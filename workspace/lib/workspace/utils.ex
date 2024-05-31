52b3ed177a14ce12c8f3242b7c42ed1115077c39
pnezis <pnezis@gmail.com>
2024-05-31 14:05:16 +0300

    Add a format_duration helper

--- workspace/lib/workspace/utils.ex
+++ workspace/lib/workspace/utils.ex
@@ -46,4 +46,42 @@ defmodule Workspace.Utils do
     """
     |> String.trim()
   end
+
+  @doc """
+  Formats duration as string
+  """
+  @spec format_duration(duration :: non_neg_integer(), unit :: :second | :millisecond) ::
+          String.t()
+  def format_duration(duration, unit \\ :millisecond)
+
+  def format_duration(duration, _unit) when is_integer(duration) and duration < 0 do
+    raise ArgumentError, "duration must be a non negative integer, got: #{duration}"
+  end
+
+  def format_duration(duration, :second) when is_integer(duration),
+    do: format_duration_from_ms(duration * 1000)
+
+  def format_duration(duration, :millisecond) when is_integer(duration),
+    do: format_duration_from_ms(duration)
+
+  defp format_duration_from_ms(duration) do
+    hour_ms = 60 * 60 * 1_000
+    {hours, duration} = {div(duration, hour_ms), rem(duration, hour_ms)}
+
+    minute_ms = 60 * 1_000
+    {minutes, duration} = {div(duration, minute_ms), rem(duration, minute_ms)}
+
+    seconds = duration / 1_000
+
+    [
+      time_unit(hours, "h"),
+      time_unit(minutes, "m"),
+      time_unit(Float.round(seconds, 2), "s")
+    ]
+    |> Enum.reject(&is_nil/1)
+    |> Enum.join(" ")
+  end
+
+  defp time_unit(0, _suffix), do: nil
+  defp time_unit(num, suffix), do: "#{num}#{suffix}"
 end
--- workspace/test/workspace/utils_test.exs
+++ workspace/test/workspace/utils_test.exs
@@ -52,4 +52,31 @@ defmodule Workspace.UtilsTest do
       assert Utils.digraph_to_mermaid(graph) == expected
     end
   end
+
+  describe "format_duration/2" do
+    test "raises with negative durations" do
+      assert_raise ArgumentError, "duration must be a non negative integer, got: -100", fn ->
+        Utils.format_duration(-100)
+      end
+    end
+
+    test "formats duration" do
+      # seconds only
+      assert Utils.format_duration(20, :second) == "20.0s"
+      assert Utils.format_duration(20_235, :millisecond) == "20.23s"
+
+      # minutes
+      assert Utils.format_duration(164, :second) == "2m 44.0s"
+      assert Utils.format_duration(164_235, :millisecond) == "2m 44.23s"
+
+      # hours
+      assert Utils.format_duration(7432, :second) == "2h 3m 52.0s"
+      assert Utils.format_duration(7_164_235, :millisecond) == "1h 59m 24.23s"
+
+      # edge cases
+      assert Utils.format_duration(0, :second) == "0.0s"
+      assert Utils.format_duration(60, :second) == "1m 0.0s"
+      assert Utils.format_duration(3600, :second) == "1h 0.0s"
+    end
+  end
 end
