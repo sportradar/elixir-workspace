96cbb0a98f60288eb93f2ee047bc3615526b0be9
pnezis <pnezis@gmail.com>
2025-10-13 13:14:53 +0300

    Release workspace 0.3.0

--- workspace/mix.exs
+++ workspace/mix.exs
@@ -2,7 +2,7 @@ defmodule Workspace.MixProject do
   use Mix.Project
 
   @app :workspace
-  @version "0.2.2"
+  @version "0.3.0"
   @repo_url "https://github.com/sportradar/elixir-workspace"
 
   def project do
